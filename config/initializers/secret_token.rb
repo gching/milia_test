434475b13f24fb6075848659b3e3ddf87f7cc7a2
Gavin Ching <gavinchingy@gmail.com>
2013-09-04 15:51:38 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiliaTest::Application.config.secret_key_base = 'a17b377b652ab7bf4396ffbc002ac42d6342f9ca5dd568c7fee0064227db940f64af0896fcd531c0fd0c63eb09bb7cf5101e0554e6d49f626a0fd2440f3e2960'
